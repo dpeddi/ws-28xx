4a2dcfdb250f29da125b953bb62818e7a67ba3db
Eddi De Pieri <eddi@depieri.net>
2012-03-28 10:33:31 +0200

    ws-28xx: add missing component

---
+++ CWeatherTraits.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+class CWeatherTraits(object):
+
+	def TemperatureNP(self):
+		return 81.099998
+
+	def TemperatureOFL(self):
+		return 136.0
+
+	def PressureNP(self):
+		return 10101010.0
+
+	def PressureOFL(self):
+		return 16666.5
+
+	def HumidityNP(self):
+		return 110.0
+
+	def HumidityOFL(self):
+		return 121.0
+
+	def RainNP(self):
+		return -0.2
+
+	def RainOFL(self):
+		return 16666.664
+
+	def WindNP(self):
+		return 51.0
+
+	def WindOFL(self):
+		return 51.099998
+
+	def TemperatureOffset(self):
+		return 40.0
